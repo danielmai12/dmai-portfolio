c3f4825681a972217d2df90cca2f8ee551ceb2b5
danielmai12 <danielmai122001@gmail.com>
2025-04-06 14:32:31 -0500

    Finish home page

---
+++ components/Social.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+
+import { FaGithub, FaLinkedin, FaYoutube, FaTwitter } from "react-icons/fa";
+
+const socials = [
+  { icon: <FaGithub />, path: "" },
+  { icon: <FaLinkedin />, path: "" },
+  { icon: <FaYoutube />, path: "" },
+  { icon: <FaTwitter />, path: "" },
+];
+
+/**
+ * Social component to render social media icons
+ * @param {string} containerStyles - Styles for the container
+ * @param {string} iconStyles - Styles for the icons
+ * @returns JSX.Element
+ */
+const Social = ({
+  containerStyles,
+  iconStyles,
+}: {
+  containerStyles: string;
+  iconStyles: string;
+}) => {
+  return (
+    <div className={containerStyles}>
+      {socials.map((item, index) => (
+        <Link href={item.path} key={index} className={iconStyles}>
+          {item.icon}
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Social;
